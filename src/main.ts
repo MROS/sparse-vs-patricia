3979896592c2c1383e12a34fbcdb773e3b03cb9a
MROS <yc1043@gmail.com>
2019-08-08 15:50:44 +0800

    增加 flushThenRestore 測試

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { test_suite } from './test_suite';
+import * as fs from 'fs';
 
 function main(): void {
 	for (let test_generator of test_suite) {
-		const program = test_generator();
+		const [program, name] = test_generator;
+		console.log(`寫入 test_data/${name}`);
+		let fd = fs.openSync(`test_data/${name}`, 'w');
 		for (let instruction of program) {
-			console.log(instruction.toString());
+			fs.appendFileSync(fd, instruction.toString() + '\n', 'utf8');
 		}
+		fs.closeSync(fd);
 	}
 }
 
--- src/test_suite.ts
+++ src/test_suite.ts
@@ -6,7 +6,7 @@ import {
 	flush
 } from './instruction';
 
-function simple(n: number): Instruction[] {
+function randomInsertGet(n: number): [Instruction[], string[]] {
 	const data: Instruction[] = [];
 	let key_pool: string[] = [];
 	// 第一個指令一定是 insert ，樹裡沒有資料無法 get
@@ -23,12 +23,35 @@ function simple(n: number): Instruction[] {
 			key_pool.push(insert.key);
 		}
 	}
+	return [data, key_pool];
+}
+
+function simple(): Instruction[] {
+	const data = randomInsertGet(1000)[0];
 	data.push(root);
 	return data;
 }
 
-const test_suite = [
-	() => simple(1000)
+function flushThenRestore(n: number): Instruction[] {
+	const [data, key_pool] = randomInsertGet(n);
+	data.push(root);
+	data.push(flush);
+	for (let i = 1; i < n; i++) {
+		if (Math.random() < 0.5) {
+			data.push(randomGet(key_pool));
+		} else {
+			const insert = randomInsert();
+			data.push(insert);
+			key_pool.push(insert.key);
+		}
+	}
+	data.push(root);
+	return data;
+}
+
+const test_suite: [Instruction[], string][] = [
+	[simple(), 'simple'],
+	[flushThenRestore(100000), 'flushThenRestore-100000']
 ];
 
 export {
